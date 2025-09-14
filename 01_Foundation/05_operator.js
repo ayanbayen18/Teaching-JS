f5b23e6aa902add356928f4293f6442801e9aea8
Ayan Bayen <ayanbayen21@gmail.com>
2025-09-14 20:04:13 +0530

    add operator in js

---
+++ 01_Foundation/05_operator.js
@@ -0,0 +1,69 @@
+// Operator -> Operator holo akta special symbol ba keyword javascript er, jeta amra use kori kono operation korar jonno dui value er moddhe ba dui operand er moddhe. 
+
+// amra operator use kori calculation korte, comperison korte, logic banate, assignment korte, and type check korte. 
+
+// Arithmetic, comperison operator, logical operator, assignment operator, typeof operator 
+
+// Arithmetic -> +, -, *, /, %, **
+// 10 / 2 -> 5
+// 10 % 2 -> 0
+// 2**3 -> 8
+
+// let a = 10; 
+// let b = 3;
+
+// console.log( a + b);
+// console.log(a - b);
+// console.log(a % b);
+// console.log(a / b);
+// console.log(a ** b);
+
+
+// Assignment Operators -> = , +=, -=, *=, /=, %=
+
+// Comparison Operators -> ==, ===, !=, !==, < , > , <=, >=
+
+// 5 == 5;
+// 5 === "5";
+// 10 > 5;
+// 10 < 5;
+// 5 >= 2;
+// 10 <= 20;
+
+// Logical Operator: && , ||, !
+
+// && -> true true -> true
+// false true -> false
+// false false -> false
+
+// -> false false -> false
+// true false -> true
+// true true -> true
+// false true -> true 
+
+// let age = 15, hasID = true;
+// if(age >= 18 && hasID){
+//     console.log("Allowd");
+// } else{
+//     console.log("not allowd");
+    
+// }
+
+
+// Unary Operator -> + , - , ++ , -- , typeof
+// ++ a -> pre increament, a++ -> post increament
+// --a -> pre decrement, a-- -> post decreament 
+
+//  Ternary -> condition ? printThis : printThisNow
+
+// let score = 20;
+// let grade = score > 50 ? "pass" : "Fail";
+// console.log(grade);
+
+
+
+
+
+
+
+
